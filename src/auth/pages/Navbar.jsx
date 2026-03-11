925352fb6753237bcdd784330286ba5651dfb843
SadikshyaAdhikari <sadikshyaadhikari2004@gmail.com>
2026-03-11 14:59:26 +0545

    feat: initial commit

--- src/auth/pages/Navbar.jsx
+++ src/auth/pages/Navbar.jsx
@@ -1,20 +1,20 @@
 //Navbar with home, login, register links
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from "@/components/ui/button"
+
 
 export function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
-        <div className="container mx-auto flex items-center justify-between">
-            <Link to="/" className="text-white text-lg font-bold">MyApp</Link>
-            <div>
-                {/* <Link to="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link> */}
-                <Link to="/login" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</Link>
-                <Link to="/register" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</Link>
-                 <Button variant="outline">Button</Button>
-            </div>  
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to="/" className="text-white text-lg font-bold">MyApp</Link>
+        <div>
+          {/* <Link to="/" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link> */}
+          <Link to="/login" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</Link>
+          <Link to="/register" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Register</Link>
+         
         </div>
+      </div>
     </nav>
   );
 }
